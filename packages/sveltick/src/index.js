de050a4de554bc79544f36b2b9512af937003896
Adam Stádník <adam.stadnik@seznam.cz>
2024-10-23 12:11:17 +0200

    fix: re-add sveltick as a regular folder

---
+++ packages/sveltick/src/index.js
@@ -0,0 +1,44 @@
+import {
+  runPerformanceTracker, // All-in-one function
+  getPerformanceMetrics, // Track metrics manually
+  trackFirstContentfulPaint,
+  trackTimeToInteractive,
+  trackLargestContentfulPaint,
+  trackCumulativeLayoutShift,
+  trackFirstInputDelay,
+  trackInteractionToNextPaint,
+  trackTimeToFirstByte,
+  trackComponentRender,
+  checkPerformanceAlerts,
+  calculatePerformanceScore,
+  runGamification
+} from './PerformanceTracker.js'
+
+import {
+  trackAllActivities,
+  getPageViews,
+  getRouteViews,
+  getTrafficSources,
+  getUniqueVisitors
+} from './ActivityTracker.js'
+
+export {
+  runPerformanceTracker, // All-in-one function
+  getPerformanceMetrics, // Track metrics manually
+  trackFirstContentfulPaint,
+  trackTimeToInteractive,
+  trackLargestContentfulPaint,
+  trackCumulativeLayoutShift,
+  trackFirstInputDelay,
+  trackInteractionToNextPaint,
+  trackTimeToFirstByte,
+  trackComponentRender,
+  checkPerformanceAlerts,
+  calculatePerformanceScore,
+  runGamification,
+  trackAllActivities, // Track all activities at once
+  getPageViews, // Get total page views
+  getRouteViews,
+  getTrafficSources,
+  getUniqueVisitors
+}
