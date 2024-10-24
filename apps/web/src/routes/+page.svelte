28d6fd50b0d568cc8bc810281f5a24cba5a497ff
Adam Stádník <adam.stadnik@seznam.cz>
2024-10-24 09:54:35 +0200

    feat(web): adding title screen, need improvement

---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import metricsimage from "../assets/metrics.svg"
+
+</script>
+
+<div>
+    <div class="flex align-center pt-16 items-center justify-center">
+        <h1 class="pr-32"><span class="meet">Meet</span> <br /> <span class="sveltick title-font">Sveltick</span></h1>
+        <img src={metricsimage} alt="metrics-image" />
+    </div>
+
+</div>
